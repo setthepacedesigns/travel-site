26ff1eb48f3a7e2385af817ea2714e295385c2c8
Jono <jonooliver@Jonos-MacBook-Pro.local>
2017-12-26 21:49:25 +0000

    Tidying gulpfile.js

---
+++ gulp/tasks/watch.js
@@ -0,0 +1,30 @@
+var gulp = require('gulp');
+var watch = require('gulp-watch');
+var browserSync = require('browser-sync').create();
+
+gulp.task('watch', function () {
+
+  browserSync.init({
+    notify: false,
+    server: {
+      baseDir: 'app'
+    },
+  });
+
+  watch('./app/index.html', function () {
+    gulp.start('html');
+  });
+
+  watch('./app/assets/styles/**/*.css', function () {
+    gulp.start('cssInject');
+  });
+});
+
+gulp.task('cssInject', ['styles'], function () {
+  return gulp.src('./app/temp/styles/styles.css')
+    .pipe(browserSync.stream());
+});
+
+gulp.task('html', function () {
+  browserSync.reload();
+});
