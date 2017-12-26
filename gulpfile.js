162e59209399c5eb9381b49f49459f88bcadbe86
Jono <jonooliver@Jonos-MacBook-Pro.local>
2017-12-26 21:39:18 +0000

    Browser Sync Addition

--- gulpfile.js
+++ gulpfile.js
@@ -5,13 +5,14 @@ var autoprefixer = require('autoprefixer');
 var cssvars = require('postcss-simple-vars');
 var nested = require('postcss-nested');
 var cssImport = require('postcss-import');
+var browserSync = require('browser-sync').create();
 
 gulp.task('default', function () {
   console.log('hooray you completed the task');
 });
 
 gulp.task('html', function () {
-  console.log('imagine summit useful here');
+  browserSync.reload();
 });
 
 gulp.task('styles', function () {
@@ -21,11 +22,24 @@ gulp.task('styles', function () {
 });
 
 gulp.task('watch', function () {
+
+  browserSync.init({
+    notify: false,
+    server: {
+      baseDir: 'app'
+    },
+  });
+
   watch('./app/index.html', function () {
     gulp.start('html');
   });
 
   watch('./app/assets/styles/**/*.css', function () {
-    gulp.start('styles');
+    gulp.start('cssInject');
   });
+});
+
+gulp.task('cssInject', ['styles'], function () {
+  return gulp.src('./app/temp/styles/styles.css')
+    .pipe(browserSync.stream());
 });
