fea30b4dcce3c546295befcc12c74829367887a2
Jono <jonooliver@Jonos-MacBook-Pro.local>
2017-12-26 20:08:27 +0000

    Adding base and modular css structure

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ var postcss = require('gulp-postcss');
 var autoprefixer = require('autoprefixer');
 var cssvars = require('postcss-simple-vars');
 var nested = require('postcss-nested');
+var cssImport = require('postcss-import');
 
 gulp.task('default', function () {
   console.log('hooray you completed the task');
@@ -15,7 +16,7 @@ gulp.task('html', function () {
 
 gulp.task('styles', function () {
   return gulp.src('./app/assets/styles/styles.css')
-  .pipe(postcss([nested, cssvars, autoprefixer]))
+  .pipe(postcss([cssImport, nested, cssvars, autoprefixer]))
   .pipe(gulp.dest('./app/temp/styles'));
 });
 
